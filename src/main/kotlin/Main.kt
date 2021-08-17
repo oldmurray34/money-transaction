f7c0af8c18cac16178345a3581a70849977ea5d4
oldmurray34 <oldmurray34@gmail.com>
2021-08-17 08:36:02 +0300

    Added tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
-fun main() {
-    val initialAmount= 10000000
-    val cardType = "Maestro"
-    val previousTransactionsThisMonth= 1000
-
-    if (transactionRatesCalc(cardType, previousTransactionsThisMonth, initialAmount) == -1) {
-        println("Вы превысили лимит транзакции")
-    } else {
-        println(
-            "Перевод на сумму ${
-                transactionRatesCalc(
-                    cardType,
-                    previousTransactionsThisMonth,
-                    initialAmount
-                )
-            } совершен"
-        )
-    }
-}
+//fun main() {
+//    val initialAmount= 10000000
+//    val cardType = "Maestro"
+//    val previousTransactionsThisMonth= 1000
+//
+//    if (transactionRatesCalc(cardType, previousTransactionsThisMonth, initialAmount) == -1) {
+//        println("Вы превысили лимит транзакции")
+//    } else {
+//        println(
+//            "Перевод на сумму ${
+//                transactionRatesCalc(
+//                    cardType,
+//                    previousTransactionsThisMonth,
+//                    initialAmount
+//                )
+//            } совершен"
+//        )
+//    }
+//}
--- src/main/kotlin/Transaction.kt
+++ src/main/kotlin/Transaction.kt
@@ -2,7 +2,7 @@ import kotlin.math.roundToInt
 
 fun transactionRatesCalc(cardType: String = "Vk pay", previousTransactionsThisMonth: Int = 0, initialAmount: Int): Int {
     var result = 0
-    if (cardType == "Vk Pay") {
+    if (cardType == "Vk pay") {
         result = if (previousTransactionsThisMonth + initialAmount >= 4_000_000 || initialAmount >= 1_500_000) {
             -1
         } else {
